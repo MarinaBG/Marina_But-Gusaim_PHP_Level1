0a25c605b84ae5004a9a7f498a6a71684f0028dd
Marina BG <marlyakh@yandex.ru>
2019-01-17 13:01:51 +0300

    ДЗ к уроку 4

---
+++ Lesson 4/Вариант галереи без загрузки файлов/index.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css"></link>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+
+        <div id="modal_form">
+            <div class="big_img">
+                <a href="#" class="close">
+                    <svg viewBox="0 0 21.9 21.9" width="15px" height="15px">
+                        <path d="M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z"/>
+                    </svg>
+                </a>
+                <img id="bigimg" alt="no foto">
+            </div>            
+        </div>
+        
+        <div class="gallery">           
+            <h1>Галерея</h1>
+            <div class="gallery_items">
+                <?include "blocks/small_img.php"?>
+            </div>
+        </div>
+
+    </div>    
+</body>
+</html>
